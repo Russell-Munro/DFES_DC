2f608e363e5f83aacbe27ccdf671bcd93899c0a4
Russell Munro Sandbox <136714585+Russell-Munro@users.noreply.github.com>
2025-08-12 07:32:10 +0800

    Add async list and term retrieval to graph service

--- UDC.SharePointOnline.GraphService/IGraphService.cs
+++ UDC.SharePointOnline.GraphService/IGraphService.cs
@@ -1,10 +1,19 @@
-﻿using System.IO;
+using System;
+using System.Collections.Generic;
 using System.Threading.Tasks;
 
 namespace Equ.SharePoint.GraphService
 {
     public interface IGraphService
     {
-  
+        Task<IEnumerable<Dictionary<string, object>>> GetListsAsync();
+
+        Task<Dictionary<string, object>> GetListAsync(Guid id);
+
+        Task<IEnumerable<Dictionary<string, object>>> GetDocumentsAsync(Guid listId, IEnumerable<string> fields);
+
+        Task<IEnumerable<Dictionary<string, object>>> GetTermSetsAsync();
+
+        Task<IEnumerable<Dictionary<string, object>>> GetTermsAsync(Guid termSetId);
     }
 }
